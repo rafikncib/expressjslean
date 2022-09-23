43cf1eb6ab39b68899709acc2ea35a0af40ccc2b
rafikncib <rafikncib@gmail.com>
2022-09-23 14:51:48 +0100

    learn expres v2

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ app.get('/json',function(req,res){
     if(process.env.MESSAGE_STYLE==='uppercase'){
         res.json({"message": "HELLO JSON"});
     }else{
-        res.json({"message": "Hello json"});
+        res.json({"message": "hello json"});
     }
   });
 
